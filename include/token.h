40bf743cd0d80a7dfe1f799a1fe23056a9608027
jabaclaw4 <nastya.v.krutilina@gmail.com>
2026-06-18 12:27:10 +0300

    fix

--- include/token.h
+++ include/token.h
@@ -2,22 +2,20 @@
 
 #include <string>
 
-//Типы токенов
+//типы токенов
 enum class TokenType {
-
-    // ── Ключевые слова ────────────────────────
-    TAPE,   // "лента"
+    TAPE, // "лента"
     WHILE,// "пока"
     END, // "конец"
     HAS_BIT,
     CMD_INVERT,
     CMD_ADD,
     COLON, // :
-    STRING,    //произвольная строка
+    STRING, //произвольная строка
     END_OF_FILE//конец входного потока
 };
 
-//Токен пара (тип, значение) value заполняется только для STRING а для остальных токенов value пустая строка
+//токен пара (тип, значение) value заполняется только для STRING а для остальных токенов value пустая строка
 struct Token {
     TokenType   type;
     std::string value;
